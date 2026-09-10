e7b96054aef3022ac9379752c5b6c625bd093ce4
Aaron <aalbro@utk.edu>
2026-09-10 16:42:09 -0400

    reducing saves

--- web/modules/custom/enable_pathauto/src/Plugin/Action/EnablePathautoState.php
+++ web/modules/custom/enable_pathauto/src/Plugin/Action/EnablePathautoState.php
@@ -12,42 +12,44 @@ use Drupal\node\NodeInterface;
 use Drupal\pathauto\PathautoState;
 
 #[Action(
-  id: 'enable_pathauto_enable_automatic_alias',
-  label: new TranslatableMarkup('Enable automatic URL alias'),
-  type: 'node',
-  category: new TranslatableMarkup('Pathauto'),
+    id: 'enable_pathauto_enable_automatic_alias',
+    label: new TranslatableMarkup('Enable automatic URL alias'),
+    type: 'node',
+    category: new TranslatableMarkup('Pathauto'),
 )]
-final class EnablePathautoState extends ActionBase {
+final class EnablePathautoState extends ActionBase
+{
 
-  /**
-   * {@inheritdoc}
-   */
-  public function execute($entity = NULL): void {
-    if (!$entity instanceof NodeInterface || !$entity->hasField('path')) {
-      return;
+    /**
+     * {@inheritdoc}
+     */
+    public function execute($entity = NULL): void
+    {
+        if (!$entity instanceof NodeInterface || !$entity->hasField('path')) {
+            return;
+        }
+
+        foreach (array_keys($entity->getTranslationLanguages()) as $langcode) {
+            $translation = $entity->getTranslation($langcode);
+            $path_field = $translation->get('path');
+
+            // This is the value submitted by path[0][pathauto] on the node form.
+            if ($path_field->isEmpty()) {
+                $path_field->appendItem();
+            }
+            $path_field->first()->set('pathauto', PathautoState::CREATE);
+        }
+
+        $entity->save();
     }
 
-    foreach ($entity->getTranslationLanguages() as $langcode => $language) {
-      $translation = $entity->getTranslation($langcode);
-      $path_field = $translation->get('path');
-
-      // This is the value submitted by path[0][pathauto] on the node form.
-      if ($path_field->isEmpty()) {
-        $path_field->appendItem();
-      }
-      $path_field->first()->set('pathauto', PathautoState::CREATE);
-
-      $translation->save();
+    /**
+     * {@inheritdoc}
+     */
+    public function access($object, ?AccountInterface $account = NULL, $return_as_object = FALSE)
+    {
+        return $object instanceof NodeInterface
+            ? $object->access('update', $account, $return_as_object)
+            : FALSE;
     }
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function access($object, ?AccountInterface $account = NULL, $return_as_object = FALSE) {
-    return $object instanceof NodeInterface
-      ? $object->access('update', $account, $return_as_object)
-      : FALSE;
-  }
-
 }
